aa374af1a3117c068cd913045746cb98577d9b81
Chrystiomar <chrystiomar@hotmail.com>
2022-06-18 08:53:05 -0300

    incluindo conceitos de rotas

---
+++ src/app/pipe/repleace.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: "replace"
+})
+export class ReplacePipe implements PipeTransform{
+
+        transform(value: string, char: string, valueToReplacer: string) {
+            return value.replace(char, valueToReplacer);
+        }
+}
